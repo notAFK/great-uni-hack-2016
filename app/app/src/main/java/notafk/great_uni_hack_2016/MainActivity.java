265eddcd0d36a4ff3e4e748bfe0802dd7449596b
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-13 02:33:28 +0000

    Intent intent intent.

--- app/app/src/main/java/notafk/great_uni_hack_2016/MainActivity.java
+++ app/app/src/main/java/notafk/great_uni_hack_2016/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.ForwardingListener;
 import android.text.InputType;
 import android.util.Log;
 import android.widget.Button;
@@ -34,8 +35,41 @@ import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String givenPassword = "";
+    public int notifCounter = 5;
+
+    final Intent intent = new Intent(this, Fpassword.class);
+
+    protected void showSimplePopUp() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("PASSWORD PLEASE:");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                givenPassword = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    // OTHER TOGGLE
+showSimplePopUp();
                 } else {
                     // The toggle is disabled
                 }
@@ -93,7 +127,15 @@ public class MainActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mNotificationManager.notify(1, mBuilder.build());
+                mNotificationManager.notify(notifCounter, mBuilder.build());
+                notifCounter++;
+            }
+        });
+
+        final Button secondbutton = (Button) findViewById(R.id.button2);
+        secondbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showSimplePopUp();
             }
         });
 
